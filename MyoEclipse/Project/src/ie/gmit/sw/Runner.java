52c78c6183e573d790edc763de8c731d890a91b5
Emmanuel-Osabuehien <g00373559@gmit.ie>
2022-04-28 12:38:24 +0100

    Adding Required Jar Files and Compiling The Runner Class

--- MyoEclipse/Project/src/ie/gmit/sw/Runner.java
+++ MyoEclipse/Project/src/ie/gmit/sw/Runner.java
@@ -1,5 +1,33 @@
 package ie.gmit.sw;
 
-public class Runner {
+import java.awt.FontFormatException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import javax.swing.JFrame;
+
+public class Runner extends JFrame implements ActionListener{
+
+	private static final long serialVersionUID = 1L;
+
+	public Runner() {
+		super("Gesture Based UI Project");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLayout(null);
+        setSize(770, 650);
+
+        setResizable(false);
+        setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		
+	}
+
+	public static void main(String[]args) throws IOException, FontFormatException{
+        new Runner();
+    }
 }
