6934bff81274af632fd3e4f180e404ca3f5db847
Emmanuel-Osabuehien <g00373559@gmit.ie>
2022-05-02 16:14:38 +0100

    Adding Blocks and Weapons For Game

--- MyoEclipse/Project/src/ie/gmit/sw/GameScreen.java
+++ MyoEclipse/Project/src/ie/gmit/sw/GameScreen.java
@@ -5,15 +5,23 @@ import java.awt.Graphics2D;
 
 import ie.gmit.sw.Statator;
 import ie.gmit.sw.Player;
+import ie.gmit.sw.Blocks;
 
 public class GameScreen implements Statator{
 	
 	//player cannon
 		private Player cannon;
+		
+		// this is the height and widgth of the canvas 
+		private float canvasWidth = 770;
+		private float canvasHeight = 652;
+		
+		private Blocks blocks;
 
 		public GameScreen() {
 			// send the constructor the positions 
-			cannon = new Player(150, 150, 50, 50);
+			cannon = new Player(canvasWidth/2-50,canvasHeight-100 , 50, 50);
+			blocks = new Blocks();
 		}
 
 	@Override
@@ -24,6 +32,7 @@ public class GameScreen implements Statator{
 	@Override
 	public void draw(Graphics2D g) {
 		cannon.draw(g);
+		blocks.draw(g);
 	}
 
 	@Override
--- MyoEclipse/Project/src/ie/gmit/sw/Player.java
+++ MyoEclipse/Project/src/ie/gmit/sw/Player.java
@@ -10,6 +10,8 @@ import java.net.URL;
 
 import javax.imageio.ImageIO;
 
+import ie.gmit.sw.Display;
+
 public class Player implements KeyListener{
 	
 	private final double speed = 5.0d;
@@ -52,7 +54,7 @@ public class Player implements KeyListener{
 	}
 	
 	public void update(double delta){
-		if(right && !left){
+		if(right && !left && xpos < 748 - width){
 			xpos += speed * delta;
 			rect.x = (int) xpos;
 		}if(!right && left && xpos > 10){
