496c46bc1d8a1c6d9d10280a60a69eeb33c07576
Emmanuel-Osabuehien <g00373559@gmit.ie>
2022-05-02 15:52:20 +0100

    Added Cannon To Game and Movement Functions

--- MyoEclipse/Project/src/ie/gmit/sw/GameScreen.java
+++ MyoEclipse/Project/src/ie/gmit/sw/GameScreen.java
@@ -4,25 +4,31 @@ import java.awt.Canvas;
 import java.awt.Graphics2D;
 
 import ie.gmit.sw.Statator;
+import ie.gmit.sw.Player;
 
 public class GameScreen implements Statator{
+	
+	//player cannon
+		private Player cannon;
+
+		public GameScreen() {
+			// send the constructor the positions 
+			cannon = new Player(150, 150, 50, 50);
+		}
 
 	@Override
 	public void update(double delta) {
-		// TODO Auto-generated method stub
-
+		cannon.update(delta);
 	}
 
 	@Override
 	public void draw(Graphics2D g) {
-		// TODO Auto-generated method stub
-
+		cannon.draw(g);
 	}
 
 	@Override
 	public void init(Canvas canvas) {
-		// TODO Auto-generated method stub
-
+		canvas.addKeyListener(cannon);
 	}
 
 }
--- MyoEclipse/Project/src/ie/gmit/sw/Player.java
+++ MyoEclipse/Project/src/ie/gmit/sw/Player.java
@@ -1,19 +1,72 @@
 package ie.gmit.sw;
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
 
 public class Player implements KeyListener{
+	
+		// player sprite 
+		private BufferedImage player;
 
+		//variables
+		private double xpos, ypos;
+		private int width, height;
 
+		//rectangle
+		private Rectangle rect;
+		
+		// this is for movement and shooting
+		private boolean left = false;
+		private boolean	right = false;
+		private boolean	shoot = false;
+
+		//constructor 
+		public Player(double xpos, double ypos, int width, int height) {
+			this.xpos = xpos;
+			this.ypos = ypos;
+			this.height = height;
+			this.width = width;
+
+			//make a new rectangle 
+			rect = new Rectangle((int)xpos, (int)ypos, width, height);
+			try {
+				//open the cannon image 
+				player = ImageIO.read(new File("./Images/cannon.png"));
+
+			} catch (Exception e) {
+				
+			}
+		}
+s
 	public void draw(Graphics2D g) {
+		g.drawImage(player,(int) xpos,(int) ypos, width, height, null);
+	}
+	
+	public void update(double delta){
 
 	}
 
 	@Override
 	public void keyPressed(KeyEvent arg0) {
-
+		int key = arg0.getKeyCode();
+		// if the right key is pressed the right = true
+		if(key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT){
+			right = true;
+			// if the left key is pressed then left is true 
+		}else if (key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT){
+			left = true;
+		}
+		// space for shooting 
+		if (key == KeyEvent.VK_SPACE){
+			shoot = true;
+		}
 	}
 
 	@Override
