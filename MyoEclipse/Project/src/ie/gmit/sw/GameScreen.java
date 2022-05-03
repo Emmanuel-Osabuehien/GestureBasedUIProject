fa95b3cebcd20a776ff85a0dc370acfbca45b91a
Emmanuel-Osabuehien <g00373559@gmit.ie>
2022-05-03 12:09:59 +0100

    Deleted Previous Files and Redid Project To Fix Errors, Adding Voice Commands Too

--- MyoEclipse/Project/src/ie/gmit/sw/GameScreen.java
+++ MyoEclipse/Project/src/ie/gmit/sw/GameScreen.java
@@ -1,50 +1,190 @@
 package ie.gmit.sw;
 
-import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
 
-import ie.gmit.sw.Statator;
-import ie.gmit.sw.Player;
-import ie.gmit.sw.Blocks;
-import ie.gmit.sw.LevelOne;
+import javax.swing.JPanel;
 
-public class GameScreen implements Statator{
-	
-	//player cannon
-		private Player cannon;
-		
-		// this is the height and widgth of the canvas 
-		private float canvasWidth = 770;
-		private float canvasHeight = 652;
-		
-		private Blocks blocks;
+import ie.gmit.sw.Enemy.Enemies;
 
-		private LevelOne level;
+// JPanel containing the main menu
+public class GameScreen extends JPanel implements KeyListener {
 
-		public GameScreen(){
-			//instaisiate block()
-			blocks = new Blocks();
-			// send the constructor the positions 
-			cannon = new Player(canvasWidth/2-50,canvasHeight-100 , 50, 50, blocks);
-			level = new LevelOne(cannon);
+	private static final long serialVersionUID = 1L;
+
+	// variables
+	private boolean startGame = false;
+	private long score = 0;
+
+	private Color pOverlay = new Color(0, 0, 0, 200);
+
+	// flag for when user pauses/unpauses game
+	private boolean paused = false;
+	private boolean startAgain = false;
+
+	// for flipping
+	private int beatCount = 0;
+	private int beat = 0;
+	private Enemies[] aliens = { new Enemies(4, 75, 230), new Enemies(1, 88, 283), new Enemies(2, 83, 336), new Enemies(3, 82, 389) };
+
+	// fonts
+	private Font fontL = new Font("TimesRoman", Font.TRUETYPE_FONT | Font.ITALIC, 80);
+	private Font fontM = new Font("TimesRoman", Font.TRUETYPE_FONT | Font.ITALIC, 60);
+	private Font fontS = new Font("TimesRoman", Font.TRUETYPE_FONT | Font.ITALIC, 40);
+
+	// main menu
+	public GameScreen() throws IOException, FontFormatException {
+		super();
+		voce.SpeechInterface.init("./resources", false, true, "./resources", "start");
+		setSize(800, 700);
+		addKeyListener(this);
+
+	}
+
+	public void addNotify() {
+		super.addNotify();
+		requestFocus();
+	}
+
+	public boolean getStatus() {
+		return startGame;
+	}
+
+	public boolean getPauseStatus() {
+		return paused;
+	}
+
+	public boolean startOver() {
+		// TODO Auto-generated method stub
+		return startAgain;
+	}
+
+	// reset the start game boolean to false after restarting
+	public void setStartOver(boolean b) {
+		// TODO Auto-generated method stub
+		this.startAgain = b;
+	}
+
+	public void setHiScore(long getScore) {
+		score = getScore;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// check if p has been pressed
+		if (e.getKeyCode() == KeyEvent.VK_P) {
+			System.out.println("P pressed for start");
+			startGame = true;
+		}
+	}
+
+	// for startint the game and quiting the menu
+	public void sppech() {
+
+		System.out.println("Say Start!");
+
+		boolean start = false;
+		while (!start) {
+			// Normally, applications would do application-specific things
+			// here. For this sample, we'll just sleep for a little bit.
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+			}
+			while (voce.SpeechInterface.getRecognizerQueueSize() > 0) {
+				String s = voce.SpeechInterface.popRecognizedString();
+
+				// check if s is equal to start
+				if (s.contentEquals("start")) {
+					// if it is then end the loop
+					// start = true;
+					// start the game
+					startGame = true;
+				} else if (s.contentEquals("stop")) {
+					paused = true;
+				} else if (s.contentEquals("continue")) {
+					paused = false;
+				} else if (s.contentEquals("reset")) {
+					startAgain = true;
+				} else {
+					s = "empty";
+				}
+				System.out.println("You said: " + s);
+			}
+
 		}
 
-	@Override
-	public void update(double delta) {
-		cannon.update(delta);
-		level.update(delta, blocks);
+		voce.SpeechInterface.destroy();
+
 	}
 
-	@Override
-	public void draw(Graphics2D g) {
-		cannon.draw(g);
-		blocks.draw(g);
-		level.draw(g);
-	}
+	public void paintComponent(Graphics g) {
+		// this counts how many times looped
+		beatCount += 1;
 
-	@Override
-	public void init(Canvas canvas) {
-		canvas.addKeyListener(cannon);
-	}
+		if (beatCount % 70 == 0) {
+			// move aliens when counter is divisible by beat modifier
+			beat = 1 - beat;
+			// flips between 1 and 0
+			beatCount = 0;
+		}
 
+		// menu background set color and fill
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, 800, 700);
+
+		// Set the text here
+		Graphics2D comp2D = (Graphics2D) g;
+		// chose the colour
+		g.setColor(Color.WHITE);
+		comp2D.setFont(fontL);
+		// name of the project or title
+		comp2D.drawString("Gesture Project", 83, 160);
+		comp2D.setFont(fontM);
+
+		// this is used to draw the alien images for x and y pos
+		int posX = 120;
+		int posY = 270;
+
+		// Initialize a colour var
+		Color c;
+
+		// here get the alien images from the array
+		// and draw them
+		for (int i = 1; i < 4; i++) {
+			// get the image
+			Image alien = aliens[i].getImage(beat);
+
+			// this just sets the background colour of the alien images
+			if (i == 2) {
+				c = Color.RED;
+			} else if (i == 3) {
+				c = Color.GREEN;
+			} else {
+				c = Color.BLUE;
+			}
+
+			// Separate the image by multiplying the x-axis by 120 to separate them
+			g.drawImage(alien, posX + (i * 120), posY, c, null);
+		}
+
+		// other text
+		comp2D.drawString("Say Start", 250, 420);
+
+	}
 }
--- MyoEclipse/Project/src/ie/gmit/sw/Runner.java
+++ MyoEclipse/Project/src/ie/gmit/sw/Runner.java
@@ -7,27 +7,197 @@ import java.io.IOException;
 
 import javax.swing.JFrame;
 
-public class Runner extends JFrame implements ActionListener{
+import ie.gmit.sw.Block.Blocks;
+import ie.gmit.sw.Bullet.Bullets;
+import ie.gmit.sw.Enemy.Aliens;
+import ie.gmit.sw.Player.Player;
+import ie.gmit.sw.Score.ScoreBoard;
+
+public class Runner extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 
-	public Runner() {
-		super("Gesture Based UI Project");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	private javax.swing.Timer myTimer;
 
-        setLayout(null);
-        setSize(770, 650);
+	// first JPanel
+	private static GameScreen menu;
+	private boolean gameStart = false;
 
-        setResizable(false);
-        setVisible(true);
+	// check if game is paused
+	private boolean paused = false;
+	// start again
+	private boolean startAgain;
+
+	// component classes of the game
+	private static Controller overseer;
+	private Player player;
+	private Aliens enemies;
+	private ScoreBoard score;
+	private Blocks shield;
+	private Bullets shotsFired;
+	// number of wins by the user, keeps track of subsequent alien start location
+	private int wave = 0;
+
+	public Runner() throws IOException, FontFormatException {
+		super("GUI Project");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		setLayout(null);
+		setSize(770, 652);
+
+		menu = new GameScreen();
+		score = new ScoreBoard();
+
+		menu.setHiScore(score.getHiScore());
+
+		// add menu to the JFrame
+		add(menu);
+
+		// update every 10 ms
+		myTimer = new javax.swing.Timer(10, this);
+		myTimer.start();
+
+		setResizable(false);
+		setVisible(true);
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent ae) {
-		
+	private void nextLevel() throws IOException, FontFormatException {
+		// called every time user wins (all aliens destroyed), resets game setup
+		remove(overseer);
+		if (wave < 10) {
+			wave += 1;
+		}
+		player.addLife();
+		enemies = new Aliens(wave, score, player, shield);
+		shotsFired = new Bullets(player, enemies, shield);
+		overseer = new Controller(player, enemies, score, shield, shotsFired);
+		add(overseer);
 	}
 
-	public static void main(String[]args) throws IOException, FontFormatException{
-        new Runner();
-    }
+	private void startOverGame() throws IOException, FontFormatException {
+
+		if (overseer != null) {
+			remove(overseer);
+		}
+
+		wave = 0;
+		player = new Player();
+		shield = new Blocks();
+		score.setShip(player);
+		score.resetScore();
+		enemies = new Aliens(wave, score, player, shield);
+		shotsFired = new Bullets(player, enemies, shield);
+		overseer = new Controller(player, enemies, score, shield, shotsFired);
+		add(overseer);
+	}
+
+	// event listener stuff, update classes every 10 ms
+	public void actionPerformed(ActionEvent evt) {
+
+		Object source = evt.getSource();
+		// check if pasued or not
+		paused = menu.getPauseStatus();
+
+		if (source == myTimer) {
+			if (gameStart) {
+				if (overseer.stillPlaying() && !overseer.isPaused() && !player.gotHit()) {
+					// only move when not paused and player still alive
+					// move player
+					overseer.move();
+					// if aliens have moved
+					if (enemies.metronome()) { 
+						// launch attack
+						shotsFired.setAlienShots(enemies.attack());
+					}
+					// move mystery UFO regardless of beat
+					enemies.ufoTrack();
+					// move shots if they exist
+					shotsFired.trackBullets();
+				}
+
+				if (!overseer.stillPlaying()) {
+
+					// start again
+					startAgain = menu.startOver();
+
+					// delete everything
+					enemies.ufoDestroy();
+					score.calculateHiScore();
+
+					// start again
+					if (startAgain) {
+						try {
+							menu.setStartOver(false);
+							startOverGame();
+						} catch (IOException | FontFormatException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+
+				}
+
+				overseer.repaint();
+
+				if (enemies.aliensGone()) { // if no aliens left
+					// error handling in case font doesn't exist
+					try {
+						nextLevel();
+					} catch (IOException e) {
+						e.printStackTrace();
+					} catch (FontFormatException e) {
+						e.printStackTrace();
+					}
+				}
+
+				if (overseer.doRestartGame()) { // check if player wants to restart game
+					// error handling in case font doesn't exist
+					try {
+						startOverGame();
+					} catch (IOException e) {
+						e.printStackTrace();
+					} catch (FontFormatException e) {
+						e.printStackTrace();
+					}
+				}
+
+				// this here pauses the game but at the moment its very buggy
+				// resume the game
+				if (paused) {
+					overseer.setPaused(true);
+					overseer.repaint();
+				}
+				if (!paused) {
+					overseer.setPaused(false);
+					overseer.repaint();
+				}
+			} else {
+				gameStart = menu.getStatus();
+				// initialize if player starts game
+				if (gameStart) {
+					remove(menu);
+
+					// error handling in case font doesn't exist
+					try {
+						startOverGame();
+					} catch (IOException e) {
+						e.printStackTrace();
+					} catch (FontFormatException e) {
+						e.printStackTrace();
+					}
+				}
+
+				menu.repaint();
+			}
+
+		}
+	}
+
+	public static void main(String[] args) throws IOException, FontFormatException {
+		new Runner();
+
+		// run the speech recognition to start the game
+		menu.sppech();
+
+	}
 }
